2fb4d3143f574521742220bb89f0b1e00d94791e
dengzhi3 <1174954599@qq.com>
2026-08-22 23:54:37 +0800

    Release YOLO-PMB reproducible version

---
+++ Supporting Information/train.py
@@ -0,0 +1,29 @@
+
+import warnings, os
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"    # 代表用cpu训练 不推荐！没意义！ 而且有些模块不能在cpu上跑
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"     # 代表用第一张卡进行训练  0：第一张卡 1：第二张卡
+# 多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+
+
+if __name__ == '__main__':
+    model = YOLO('configs/yolopmb.yaml')
+    # model.load('yolo11n.pt') # loading pretrain weights
+    model.train(data='dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=200,
+                batch=8,
+                close_mosaic=0,
+                workers=8, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
+                # device='0,1', # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
+                optimizer='SGD', # using SGD
+                # patience=0, # set 0 to close earlystop.
+                # resume=True, # 断点续训,YOLO初始化时选择last.pt
+                amp=False, # close amp | loss出现nan可以关闭amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
